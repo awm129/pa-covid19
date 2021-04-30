f1b13df6f34f221622fe075b46b68ec753777e48
andy <awm129@gmail.com>
2021-04-30 09:46:17 -0400

    initial commit
    Bash script to automate pulling down COVID-19 data from the Pennsylvania
    Department of Health's website. The PA DoH is really bad at data
    consistency, so the posted pdf's filename can be one of several
    different mutations. This script tries all of the mutations.

---
+++ update.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+
+function get_pdf()
+{
+	local localfile="$1".pdf
+	if [[ -e ${localfile} ]]; then
+		if [[ ! -s ${localfile} ]]; then
+			echo "removing empty file ${localfile}"
+			rm "${localfile}"
+		else
+			return 0
+		fi
+	fi
+	
+	local notify=0
+	local url="https://www.health.pa.gov/topics/Documents/Diseases%20and%20Conditions/COVID-19%20County%20Data"
+	local flags="-O ${localfile}"
+	local fileroot=(
+		"County%20Case%20Counts_"
+		"County%20Case%20Counts%20"
+		"County%20Counts_"
+		"County%20Counts%20"
+	)
+
+	if [[ ${VERBOSE} -eq 0 ]]; then
+		flags="-q ${flags}"
+	fi
+	
+	#
+	# PA DoH is bad at data consistency. The filenames may or may not have
+	# leading 0s for month and day. Sometimes they use underscores,
+	# sometimes hyphens. Test all possibilities.
+	#
+
+	local dates=(
+		"${1}"				# date mm-dd-yyyy
+		"${1##0}"				# no leading 0 in month
+		"${1/-0/-}"			# no leading 0 in day
+	)
+
+	dates+=(
+		"${dates[1]/-0/-}"	# no leading 0 in month or day
+	)
+
+	dates+=(
+		"${dates[0]//-/_}"	# date mm_dd_yyyy
+		"${dates[1]//-/_}"	# no leading 0 in month w/ underscores
+		"${dates[2]//-/_}"	# no leading 0 in day w/ underscores
+		"${dates[3]//-/_}"	# no leading 0s w/ underscores
+		"${dates[0]//-/ }"	# date mm dd yyyy
+		"${dates[1]//-/ }"	# no leading 0 in month w/ spaces
+		"${dates[2]//-/ }"	# no leading 0 in day w/ spaces
+		"${dates[3]//-/ }"	# no leading 0s w/ spaces
+	)
+
+	local filename=""
+
+	while true; do
+		for base in "${fileroot[@]}"; do
+			for date in "${dates[@]}"; do
+				filename="${base}${date}.pdf"
+				if ! wget ${flags} "${url}/${filename}"; then
+					rm "$localfile"
+				else
+					if [[ $VERBOSE -eq 1 ]]; then
+						echo "${url}/${filename}"
+					fi
+					# found it! break all the way out of the while
+					break 3
+				fi
+			done
+		done
+
+		if [[ $notify -eq 0 ]]; then
+			if [[ $(date +"%m-%d-%Y") == "$1" ]]; then
+				echo "waiting on $1..."
+				notify=1
+			elif [[ $(date -d"${1//-/\/}" +%w) -eq 0 ]]; then
+				echo "waiting for Sunday ($1) results..."
+				notify=1
+			else
+				echo "couldn't find $1"
+				return 1
+			fi
+		fi
+
+		sleep 60
+	done
+
+	if [[ $notify -eq 1 ]]; then
+		echo "${filename}"
+		date
+		(aplay -q NOTIFY.WAV &)
+	fi
+}
+
+function split_line()
+{
+	local -n cases=$2
+	local -n confirmed=$3
+	local -n probable=$4
+	local -n negative=$5
+
+	cases=$(echo "$1" | awk '{print $3}')
+	confirmed=$(echo "$1" | awk '{print $4}')
+	probable=$(echo "$1" | awk '{print $5}')
+	negative=$(echo "$1" | awk '{print $6}')
+
+	#
+	# strip any commas from the numbers
+	#
+	cases=${cases//,/}
+	confirmed=${confirmed//,/}
+	probable=${probable//,/}
+	negative=${negative//,/}
+}
+
+function update_covid19()
+{
+	d=$(date --date "$1" "+%m-%d-%Y")
+	d0=$(date --date="${d//-/\/} - 1 day" "+%m-%d-%Y")
+
+	declare today_cases
+	declare today_confirmed
+	declare today_probable
+	declare today_negative
+
+	declare yesterday_cases
+	declare yesterday_confirmed
+	declare yesterday_probable
+	declare yesterday_negative
+
+	local new_case_count=""
+	
+	if get_pdf "${d0}"; then
+		local yesterday_centre
+		yesterday_centre=$(pdfgrep -i "centre" "${d0}".pdf)
+		split_line "$yesterday_centre" yesterday_cases yesterday_confirmed yesterday_probable yesterday_negative
+	fi
+
+	if get_pdf "$d"; then
+		local today_centre
+		today_centre=$(pdfgrep -i "centre" "$d".pdf)
+		split_line "$today_centre" today_cases today_confirmed today_probable today_negative
+	fi
+
+	if [[ ! -e $d.pdf ]]; then
+		return 1
+	fi
+
+	if [[ -e $d0.pdf ]]; then
+		new_case_count="($((today_cases - yesterday_cases)))"
+	fi
+
+	if [[ ${HEADER} -eq 1 ]]; then
+		printf "date\t\ttotal cases\tconfirmed\tprobable\tnegative\tchange since yesterday\n"
+		printf "==========\t===========\t=========\t========\t========\t======================\n"
+		HEADER=0
+	fi
+
+	printf "%s\t%i\t\t%i\t\t%i\t\t%i\t\t%s\n" "$d" "$today_cases" "$today_confirmed" "$today_probable" "$today_negative" "$new_case_count"
+}
+
+VERBOSE=0
+RECURSIVE=0
+HEADER=0
+
+function usage()
+{
+	echo "update.sh [-r] [-v] [-h] [date]"
+	echo "	-r		fetch data recursively from the given date until today"
+	echo "	-v		verbose logging"
+	echo "	-h		print column headers"
+}
+
+opts='rvh'
+while getopts $opts op
+do
+	case $op in
+		r) RECURSIVE=1;;
+		v) VERBOSE=1;;
+		h) HEADER=1;;
+		?) usage; exit;;
+		:) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
+update_covid19 "$1";
+
+if [[ $RECURSIVE == 1 ]]; then
+	dt=$1
+	today=$(date +%s)
+	while true; do
+		dt=$(date --date "$dt + 1 day" "+%m/%d/%Y")
+		if [[ $(date -d "$dt" +%s) -gt $today ]]; then
+			break
+		fi
+		update_covid19 "$dt"
+	done
+fi
